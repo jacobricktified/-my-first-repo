465469907a37a1cc9e055f639aee060f02d8dcab
Jacob Mbuco <mbucojacob@gmail.com>
2026-09-25 13:49:45 +0300

    Fix failing test

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 console.log("Running tests...");
 const assert = require("assert");
 
-assert.strictEqual(1 + 1, 3, "Math works");
+assert.strictEqual(1 + 1, 2, "Math works");
 
 console.log("All tests passed!");
